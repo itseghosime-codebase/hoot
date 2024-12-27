d8ae7f37dd069e0297af698af3370a06de34359c
itseghosime-codebase <info.itseghosime@gmail.com>
2024-12-27 09:27:02 +0100

    Add files via upload

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -1,15 +1,7 @@
 import Quote from './../assets/MTT-Quote.png';
 import MuffedClouds from './../assets/muffedcloud.png';
 import ThreeMuffs from './../assets/threemuffs.png';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import Member from './../assets/member-bird.png';
-import BirdCTS from './../assets/CTS Bird.png';
-import CTS from './../assets/CTS.png';
-import { FaTwitter } from "react-icons/fa";
 import CloudTwo from '../assets/Cloud-two.png'
-import { LuCircleChevronDown, LuCircleChevronUp } from 'react-icons/lu';
-import { Keyboard, Navigation } from "swiper/modules";
-import React from 'react';
 import gsap from 'gsap';
 import { useGSAP } from '@gsap/react';
 import { ScrollTrigger } from "gsap/ScrollTrigger";
@@ -19,16 +11,6 @@ gsap.registerPlugin(ScrollTrigger);
 
 
 export default function Team() {
-  const SlideRef = React.useRef<any>();
-
-  const handleNext = () => {
-    SlideRef.current.swiper.slideNext();
-  };
-
-  const handlePrev = () => {
-    SlideRef.current.swiper.slidePrev();
-  };
-
   useGSAP(() => {
     gsap.from('.quote', {
       scale: 0,
@@ -64,122 +46,10 @@ export default function Team() {
         </div>
         <div className='overflow-hidden flex items-center md:items-start justify-between md:justify-center gap-5 flex-col md:flex-row'>
           <div className='bg-[#008CF4] border border-[#162936] rounded-2xl pr-4 w-full gap-3 max-w-md md:py-10 md:pl-10 py-8 pl-8'>
-            {/* <Swiper
-              ref={SlideRef}
-              keyboard={{
-                enabled: true,
-              }}
-              modules={[Keyboard, Navigation]}
-              direction={'vertical'}
-              slidesPerView={3}
-              spaceBetween={20}
-              className="mySwiper max-w-500"
-            >
-              <SwiperSlide className='max-h-24 md:max-h-32'>
-                <div className='bg-[#008CF4] border border-[#162936] rounded-2xl pl-5 md:pl-8 pr-4 flex items-center justify-between w-full gap-3'>
-                  <img src={Member} alt="Members" className='max-h-32 h-24 md:h-full w-auto' />
-                  <div>
-                    <h6 className='text-2xl md:text-3xl'>Hoot prime</h6>
-                    <p className='text-base md:text-2xl'>Founder</p>
-                  </div>
-                  <a href="#" className='hover:text-[#F89C2E] transition-colors ease-in-out duration-200'>
-                    <span className='h-10 w-10 md:h-16 md:w-16 rounded-full bg-[#63B3EE] relative border border-white flex items-center justify-center'>
-                      <FaTwitter className='text-xl md:text-3xl' />
-                    </span>
-                  </a>
-                </div>
-              </SwiperSlide>
-              <SwiperSlide className='max-h-24 md:max-h-32'>
-                <div className='bg-[#008CF4] border border-[#162936] rounded-2xl pl-5 md:pl-8 pr-4 flex items-center justify-between w-full gap-3'>
-                  <img src={Member} alt="Members" className='max-h-32 h-24 md:h-full w-auto' />
-                  <div>
-                    <h6 className='text-2xl md:text-3xl'>Hoot prime</h6>
-                    <p className='text-base md:text-2xl'>Founder</p>
-                  </div>
-                  <a href="#" className='hover:text-[#F89C2E] transition-colors ease-in-out duration-200'>
-                    <span className='h-10 w-10 md:h-16 md:w-16 rounded-full bg-[#63B3EE] relative border border-white flex items-center justify-center'>
-                      <FaTwitter className='text-xl md:text-3xl' />
-                    </span>
-                  </a>
-                </div>
-              </SwiperSlide>
-              <SwiperSlide className='max-h-24 md:max-h-32'>
-                <div className='bg-[#008CF4] border border-[#162936] rounded-2xl pl-5 md:pl-8 pr-4 flex items-center justify-between w-full gap-3'>
-                  <img src={Member} alt="Members" className='max-h-32 h-24 md:h-full w-auto' />
-                  <div>
-                    <h6 className='text-2xl md:text-3xl'>Hoot prime</h6>
-                    <p className='text-base md:text-2xl'>Founder</p>
-                  </div>
-                  <a href="#" className='hover:text-[#F89C2E] transition-colors ease-in-out duration-200'>
-                    <span className='h-10 w-10 md:h-16 md:w-16 rounded-full bg-[#63B3EE] relative border border-white flex items-center justify-center'>
-                      <FaTwitter className='text-xl md:text-3xl' />
-                    </span>
-                  </a>
-                </div>
-              </SwiperSlide>
-              <SwiperSlide className='max-h-24 md:max-h-32'>
-                <div className='bg-[#008CF4] border border-[#162936] rounded-2xl pl-5 md:pl-8 pr-4 flex items-center justify-between w-full gap-3'>
-                  <img src={Member} alt="Members" className='max-h-32 h-24 md:h-full w-auto' />
-                  <div>
-                    <h6 className='text-2xl md:text-3xl'>Hoot prime</h6>
-                    <p className='text-base md:text-2xl'>Founder</p>
-                  </div>
-                  <a href="#" className='hover:text-[#F89C2E] transition-colors ease-in-out duration-200'>
-                    <span className='h-10 w-10 md:h-16 md:w-16 rounded-full bg-[#63B3EE] relative border border-white flex items-center justify-center'>
-                      <FaTwitter className='text-xl md:text-3xl' />
-                    </span>
-                  </a>
-                </div>
-              </SwiperSlide>
-              <SwiperSlide className='max-h-24 md:max-h-32'>
-                <div className='bg-[#008CF4] border border-[#162936] rounded-2xl pl-5 md:pl-8 pr-4 flex items-center justify-between w-full gap-3'>
-                  <img src={Member} alt="Members" className='max-h-32 h-24 md:h-full w-auto' />
-                  <div>
-                    <h6 className='text-2xl md:text-3xl'>Hoot prime</h6>
-                    <p className='text-base md:text-2xl'>Founder</p>
-                  </div>
-                  <a href="#" className='hover:text-[#F89C2E] transition-colors ease-in-out duration-200'>
-                    <span className='h-10 w-10 md:h-16 md:w-16 rounded-full bg-[#63B3EE] relative border border-white flex items-center justify-center'>
-                      <FaTwitter className='text-xl md:text-3xl' />
-                    </span>
-                  </a>
-                </div>
-              </SwiperSlide>
-              <SwiperSlide className='max-h-24 md:max-h-32'>
-                <div className='bg-[#008CF4] border border-[#162936] rounded-2xl pl-5 md:pl-8 pr-4 flex items-center justify-between w-full gap-3'>
-                  <img src={Member} alt="Members" className='max-h-32 h-24 md:h-full w-auto' />
-                  <div>
-                    <h6 className='text-2xl md:text-3xl'>Hoot prime</h6>
-                    <p className='text-base md:text-2xl'>Founder</p>
-                  </div>
-                  <a href="#" className='hover:text-[#F89C2E] transition-colors ease-in-out duration-200' >
-                    <span className='h-10 w-10 md:h-16 md:w-16 rounded-full bg-[#63B3EE] relative border border-white flex items-center justify-center'>
-                      <FaTwitter className='text-xl md:text-3xl' />
-                    </span>
-                  </a>
-                </div>
-              </SwiperSlide>
-            </Swiper> */}
             <p className='max-w-2xl text-lg md:text-2xl text-stroke font-semibold'>
               $HOOT is a memecoin with no intrinsic value or expectation of financial return. There is no formal team or roadmap. The coin is for entertainment purposes only.
             </p>
           </div>
-          {/* <div className='flex md:flex-col items-center w-full md:w-fit justify-between md:h-full py-14'>
-            <button
-              className='hover:text-[#F89C2E] transition-colors ease-in-out duration-200'
-              onClick={() => handlePrev()}>
-              <LuCircleChevronUp className='text-5xl' />
-            </button>
-            <div className='flex flex-col items-center'>
-              <img src={CTS} alt="Click to Scroll" className='h-10 md:h-14 w-auto cloudy' />
-              <img src={BirdCTS} alt="Brid in Clouds" className='h-14 md:h-20 lg:h-24 w-auto cloud-nine' />
-            </div>
-            <button
-              className='hover:text-[#F89C2E] transition-colors ease-in-out duration-200'
-              onClick={() => handleNext()}>
-              <LuCircleChevronDown className='text-5xl' />
-            </button>
-          </div> */}
         </div>
       </div>
 
